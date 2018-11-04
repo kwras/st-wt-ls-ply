bd849cc2c3bc6bca92c0073d5ae2111b4316db1f
Jnaby <44756627+kwras@users.noreply.github.com>
2018-11-04 14:32:14 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js'),
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuute,");
+const client = new Discord.Client();
+
+
+
+
+ const devs = ['323160008411971585' , '' , ''];
+const adminprefix = "#";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**  ${argresult} ply .. :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**  ${argresult} wt .. :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**  ${argresult} ls .. :white_check_mark:**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/muuuuuute");
+      message.channel.sendMessage(`**  ${argresult} st .. :white_check_mark:**`)
+    }
+  if (message.content.startsWith(adminprefix + 'us')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**us ..**${argresult}** `)
+  } else
+  if (message.content.startsWith(adminprefix + 'av')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**avatar ... :** `);
+  }
+  });
+
+
+
+client.login('MzIzMTYwMDA4NDExOTcxNTg1.Dr9-dg.-Cuw-FOFOFf74HiyCqz35YO8Kw0');
